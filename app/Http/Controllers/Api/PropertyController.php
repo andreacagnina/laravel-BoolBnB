8c1763e07bc43b698a7dd1af185d70f2146a56f6
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-11-07 22:27:31 +0100

    [UPD] added server-side filter

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -8,22 +8,25 @@ use App\Models\Property;
 use App\Models\Image;
 use App\Models\Service;
 
-
 class PropertyController extends Controller
 {
-    public function index()
-    {
-        // Recupera le proprietà con le immagini e i servizi associati
-        $properties = Property::with('images', 'services')->paginate(24);
-    
+    public function index(Request $request)
+    {   
+        $types = Property::select('type')->distinct()->get();
+
+        $properties = Property::with('images', 'services')
+            ->when($request->type, function($query) use ($request) {
+                return $query->where('type', $request->type);
+            })
+            ->paginate(24);
+
         return response()->json([
             'success' => true,
             'results' => $properties,
+            'types' => $types,
         ]);
     }
     
-    
-
     public function show($slug)
     {
         $property = Property::with('images', 'services')->where('slug', $slug)->first();
