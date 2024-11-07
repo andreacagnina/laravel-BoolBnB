5787f46cc0d21bbcec05b86752a6859dada420a4
andreacagnina <amirkal@libero.it>
2024-11-07 15:11:20 +0100

    [eat] fix edit and create

--- resources/views/admin/properties/edit.blade.php
+++ resources/views/admin/properties/edit.blade.php
@@ -9,31 +9,33 @@
             @method('PUT')
 
             <!-- First Row: Title and Price -->
-            <div class="row mb-3">
+            <div class="row">
                 <div class="col-md-8">
                     <label for="title" class="form-label">Title: *</label>
                     <input type="text" name="title" id="title" value="{{ old('title', $property->title) }}"
                         class="form-control @error('title') is-invalid @enderror" required maxlength="50">
+                    <div id="error-container-title"></div>
                     @error('title')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="col-md-4">
                     <label for="price" class="form-label">Price: *</label>
-                    <div class="input-group mb-3">
+                    <div class="input-group">
                         <span class="input-group-text">&euro;</span>
                         <input type="number" name="price" id="price" value="{{ old('price', $property->price) }}"
-                            class="form-control @error('price') is-invalid @enderror" min="10" max="999999.99"
-                            step="0.01" required>
-                        @error('price')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+                            class="form-control rounded-end @error('price') is-invalid @enderror" min="10"
+                            max="999999.99" step="0.01" required>
                     </div>
+                    <div id="error-container-price"></div>
+                    @error('price')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
 
             <!-- Second Row: Type -->
-            <div class="row mb-3">
+            <div class="row">
                 <div class="col-md-12">
                     <label for="type" class="form-label">Type: *</label>
                     <select name="type" id="type" class="form-select @error('type') is-invalid @enderror" required>
@@ -44,6 +46,7 @@
                             </option>
                         @endforeach
                     </select>
+                    <div id="error-container-select"></div>
                     @error('type')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -51,36 +54,43 @@
             </div>
 
             <!-- Third Row: Cover Image -->
-            <div class="mb-3">
-                <label for="cover_image" class="form-label">Cover Image:</label><br>
-                @if ($property->cover_image && Storage::exists('public/' . $property->cover_image))
-                    <img class="img-thumbnail w-25 mb-2" src="{{ asset('storage/' . $property->cover_image) }}"
-                        alt="{{ $property->name }}">
-                @endif
-                <input type="file" name="cover_image" id="cover_image"
-                    class="form-control @error('cover_image') is-invalid @enderror">
-                @error('cover_image')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="cover_image" class="form-label">Cover Image:</label><br>
+                    @if ($property->cover_image && Storage::exists('public/' . $property->cover_image))
+                        <img class="img-thumbnail w-25 mb-2" src="{{ asset('storage/' . $property->cover_image) }}"
+                            alt="{{ $property->name }}">
+                    @endif
+                    <input type="file" name="cover_image" id="cover_image"
+                        class="form-control @error('cover_image') is-invalid @enderror">
+                    <div id="error-container-cover_image"></div>
+                    @error('cover_image')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
             </div>
 
             <!-- Fourth Row: Description -->
-            <div class="mb-3">
-                <label for="description" class="form-label">Description:</label>
-                <textarea name="description" id="description" rows="5"
-                    class="form-control @error('description') is-invalid @enderror">{{ old('description', $property->description) }}</textarea>
-                @error('description')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="description" class="form-label">Description:</label>
+                    <textarea name="description" id="description" rows="5"
+                        class="form-control @error('description') is-invalid @enderror">{{ old('description', $property->description) }}</textarea>
+                    <div id="error-container-description"></div>
+                    @error('description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
             </div>
 
             <!-- Fifth Row: Left and Right Columns -->
-            <div class="row mb-3">
+            <div class="row">
                 <!-- Left Column -->
                 <div class="col-md-6">
                     <label for="floor" class="form-label">Floor: *</label>
                     <input type="number" name="floor" id="floor" value="{{ old('floor', $property->floor) }}"
                         class="form-control @error('floor') is-invalid @enderror" required>
+                    <div id="error-container-floor"></div>
                     @error('floor')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -89,6 +99,7 @@
                     <input type="number" name="mq" id="mq" value="{{ old('mq', $property->mq) }}"
                         class="form-control @error('mq') is-invalid @enderror" min="10" max="5000" required
                         step="10">
+                    <div id="error-container-mq"></div>
                     @error('mq')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -98,13 +109,17 @@
                         value="{{ old('num_rooms', $property->num_rooms) }}"
                         class="form-control @error('num_rooms') is-invalid @enderror" min="1" max="50"
                         required>
+                    <div id="error-container-num_rooms"></div>
                     @error('num_rooms')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
 
                     <label for="num_beds" class="form-label">Number of Beds: *</label>
-                    <input type="number" name="num_beds" id="num_beds" value="{{ old('num_beds', $property->num_beds) }}"
-                        class="form-control @error('num_beds') is-invalid @enderror" min="1" max="20" required>
+                    <input type="number" name="num_beds" id="num_beds"
+                        value="{{ old('num_beds', $property->num_beds) }}"
+                        class="form-control @error('num_beds') is-invalid @enderror" min="1" max="20"
+                        required>
+                    <div id="error-container-num_beds"></div>
                     @error('num_beds')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -114,6 +129,7 @@
                         value="{{ old('num_baths', $property->num_baths) }}"
                         class="form-control @error('num_baths') is-invalid @enderror" min="0" max="5"
                         required>
+                    <div id="error-container-num_baths"></div>
                     @error('num_baths')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -121,21 +137,22 @@
 
                 <!-- Right Column -->
                 <div class="col-md-6">
-                    <div class="mb-3 position-relative">
+                    <div class="position-relative">
                         <label for="address" class="form-label">Address: *</label>
                         <input type="text" name="address" id="address"
                             value="{{ old('address', $property->address) }}"
                             class="form-control @error('address') is-invalid @enderror" required minlength="2"
                             maxlength="100" autocomplete="off">
+                        <div id="error-container-address"></div>
                         @error('address')
                             <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
-                        <div id="suggestions" class="list-group position-absolute w-100 mt-1 shadow-sm"
+                        <div id="suggestions" class="list-group position-absolute w-100 shadow-sm"
                             style="z-index: 1000;"></div>
                     </div>
 
                     <label class="form-label">Map:</label>
-                    <div id="map" style="width: 100%; height: 325px;"></div>
+                    <div id="map" style="width: 100%; height: 262px;"></div>
                 </div>
             </div>
 
@@ -143,39 +160,45 @@
             <input type="hidden" name="long" id="long" value="{{ old('long', $property->long) }}">
 
             <!-- Services Checkbox -->
-            <div class="mb-4">
-                <label class="form-label">Services:</label>
-                <div class="row">
-                    @foreach ($services->chunk(ceil($services->count() / 3)) as $serviceChunk)
-                        <div class="col-md-4">
-                            @foreach ($serviceChunk as $service)
-                                <div class="form-check d-flex align-items-center gap-3 mb-4">
-                                    @if ($errors->any())
-                                        <input type="checkbox" name="services[]" id="service_{{ $service->id }}"
-                                            value="{{ $service->id }}" class="form-check-input"
-                                            {{ in_array($service->id, old('services', [])) ? 'checked' : '' }}>
-                                    @else
-                                        <input type="checkbox" name="services[]" id="service_{{ $service->id }}"
-                                            value="{{ $service->id }}" class="form-check-input"
-                                            {{ $property->services->contains($service->id) ? 'checked' : '' }}>
-                                    @endif
-                                    <label for="service_{{ $service->id }}" class="form-check-label m-0 fw-normal">
-                                        @if ($service->icon)
-                                            <i class="{{ $service->icon }}"></i>
+            <div class="row">
+                <div class="col-12">
+                    <label class="form-label">Services:</label>
+                    <div class="row">
+                        @foreach ($services->chunk(ceil($services->count() / 3)) as $serviceChunk)
+                            <div class="col-md-4">
+                                @foreach ($serviceChunk as $service)
+                                    <div class="form-check d-flex align-items-center gap-3 mb-3">
+                                        @if ($errors->any())
+                                            <input type="checkbox" name="services[]" id="service_{{ $service->id }}"
+                                                value="{{ $service->id }}" class="form-check-input"
+                                                {{ in_array($service->id, old('services', [])) ? 'checked' : '' }}>
+                                        @else
+                                            <input type="checkbox" name="services[]" id="service_{{ $service->id }}"
+                                                value="{{ $service->id }}" class="form-check-input"
+                                                {{ $property->services->contains($service->id) ? 'checked' : '' }}>
                                         @endif
-                                        {{ $service->name }}
-                                    </label>
-                                </div>
-                            @endforeach
-                        </div>
-                    @endforeach
+                                        <label for="service_{{ $service->id }}" class="form-check-label m-0 fw-normal">
+                                            @if ($service->icon)
+                                                <i class="{{ $service->icon }}"></i>
+                                            @endif
+                                            {{ $service->name }}
+                                        </label>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @endforeach
+                    </div>
+                    <div id="error-container-services"></div>
+                    @error('services')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
 
             <!-- Availability and Submit/Back Buttons -->
-            <div class="row mb-5 mt-3">
+            <div class="row">
                 <div class="col-md-6 d-flex align-items-center">
-                    <label class="form-label d-flex align-items-center me-3">Available:</label>
+                    <label class="form-label d-flex align-items-center me-3 mb-0">Available:</label>
                     <div class="d-flex gap-3">
                         <div class="form-check d-flex align-items-center gap-2 m-0">
                             <input type="radio" name="available" id="available_yes" value="1"
@@ -188,6 +211,7 @@
                             <label for="available_no" class="form-check-label m-0">No</label>
                         </div>
                     </div>
+                    <div id="error-container-available"></div>
                     @error('available')
                         <div class="text-danger ms-2">{{ $message }}</div>
                     @enderror
