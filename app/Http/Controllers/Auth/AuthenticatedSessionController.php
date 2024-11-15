2cb83e1fee245c4bde310e826a17f0832e552f16
andreacagnina <amirkal@libero.it>
2024-11-15 13:10:35 +0100

    [feat] add link vue

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -43,6 +43,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect()->route('homepage');
+        return redirect('http://localhost:5174/');
     }
 }
